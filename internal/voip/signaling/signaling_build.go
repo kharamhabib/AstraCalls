5c3ebed037e65307d88878edc4521825c2439cac
Kharam Habib <kharamhabib@gmail.com>
2026-07-13 02:38:53 -0300

    feat: implement VoIP signaling stanza builders for call management

--- internal/voip/signaling/signaling_build.go
+++ internal/voip/signaling/signaling_build.go
@@ -109,7 +109,7 @@ func BuildAcceptStanza(ctx context.Context, sock core.VoipSocket, callID string,
 
 	return waBinary.Node{
 		Tag:   "call",
-		Attrs: waBinary.Attrs{"to": wanode.MustJID(wanode.CleanJID(peerJid.String())), "id": GenerateCallStanzaID()},
+		Attrs: waBinary.Attrs{"to": peerJid, "id": GenerateCallStanzaID()},
 		Content: []waBinary.Node{{
 			Tag:     "accept",
 			Attrs:   waBinary.Attrs{"call-id": callID, "call-creator": callCreator},
